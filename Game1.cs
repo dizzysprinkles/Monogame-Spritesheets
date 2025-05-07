9c97f66d180b2e53d8edf97ae8e1bfb280f0ec61
Zoey Hamm <339622581@GoTVDSB.ca>
2025-05-07 14:43:42 -0400

    Almost done spritesheets!

--- Game1.cs
+++ Game1.cs
@@ -12,13 +12,26 @@ namespace Monogame_Spritesheets
 
         Texture2D characterSpritesheet, rectangleTexture;
         Rectangle window;
+        KeyboardState keyboardState;
 
-        List<Rectangle> barrier;
+        List<Rectangle> barriers;
 
-        int row, columns;
+        int rows, columns;
         int frame; //Frame number (column) to be drawn
         int frames; //Number of frames in each column
         int directionRow; // Row # containing frames for current direction (up, down, left, right)
+        int leftRow, rightRow, upRow, downRow;
+        int width; //of each frame
+        int height; //of each frame
+
+        float speed; //speed of player
+        float time; // store elasped time
+        float frameSpeed; // how fast player frames transition; kinda like animation speed from summative
+
+        Vector2 playerLocation; // location of player collision sprite
+        Vector2 playerDirection; //direction vector of player!\
+
+        Rectangle playerCollisionRect, playerDrawRect;
 
 
 
@@ -36,7 +49,37 @@ namespace Monogame_Spritesheets
             _graphics.PreferredBackBufferHeight = window.Height;
             _graphics.ApplyChanges();
 
+            barriers = new List<Rectangle>();
+            barriers.Add(new Rectangle(100,100,30,150));
+            barriers.Add(new Rectangle(10, 400,300,30));
+            barriers.Add(new Rectangle(400, 200, 200, 30));
+            barriers.Add(new Rectangle(700, 450, 30, 100));
+
+            speed = 1.5f;
+
+            columns = 9;
+            rows = 4;
+            upRow = 0;
+            leftRow = 1;
+            downRow = 2;
+            rightRow = 3;
+
+            time = 0.0f;
+            frameSpeed = 0.08f;
+            frames = 9;
+            frame = 0;
+
+
+            playerLocation = new Vector2(20, 20);
+            playerCollisionRect = new Rectangle(20, 20, 20, 48);
+            playerDrawRect = new Rectangle(20, 20, 50, 65);
+            UpdatePlayerRects(); //Updates the rectangles so the hitbox surrounds our sprite's draw rect 
+
             base.Initialize();
+            directionRow = downRow; // Player will start facing down
+            width = characterSpritesheet.Width / columns;
+            height = characterSpritesheet.Height / rows;
+
         }
 
         protected override void LoadContent()
@@ -49,10 +92,26 @@ namespace Monogame_Spritesheets
 
         protected override void Update(GameTime gameTime)
         {
+            time += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (time > frameSpeed && playerDirection != Vector2.Zero)
+            {
+                time = 0f;
+                frame += 1;
+                if (frame >= frames)
+                    frame = 0;
+            }
+
+            keyboardState = Keyboard.GetState();
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            SetPlayerDirection();
+            playerLocation += playerDirection * speed;
+            UpdatePlayerRects();
+
+
 
             base.Update(gameTime);
         }
@@ -61,9 +120,52 @@ namespace Monogame_Spritesheets
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            foreach (Rectangle barrier in barriers)
+                _spriteBatch.Draw(rectangleTexture, barrier, Color.Black);
+
+            _spriteBatch.Draw(rectangleTexture, playerCollisionRect, Color.Black * 0.3f); //hit box for testing purposes
+            _spriteBatch.Draw(characterSpritesheet, playerDrawRect, new Rectangle(frame * width, directionRow * height, width, height), Color.White);
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
+        }
+
+        public void UpdatePlayerRects()
+        {
+            playerCollisionRect.Location = playerLocation.ToPoint();
+            playerDrawRect.X = playerCollisionRect.X - 15;
+            playerDrawRect.Y = playerCollisionRect.Y - 15;
+        }
+
+        private void SetPlayerDirection()
+        {
+            playerDirection = Vector2.Zero;
+            if (keyboardState.IsKeyDown(Keys.A))
+                playerDirection.X += -1;
+            if (keyboardState.IsKeyDown(Keys.D))
+                playerDirection.X += 1;
+            if (keyboardState.IsKeyDown(Keys.W))
+                playerDirection.Y += -1;
+            if (keyboardState.IsKeyDown(Keys.S))
+                playerDirection.Y += 1;
+
+            if (playerDirection != Vector2.Zero)
+            {
+                playerDirection.Normalize();
+                if (playerDirection.X < 0) // Moving left
+                    directionRow = leftRow;
+                else if (playerDirection.X > 0) // Moving right
+                    directionRow = rightRow;
+                else if (playerDirection.Y < 0) // Moving up
+                    directionRow = upRow;
+                else
+                    directionRow = downRow; // Moving down
+
+            }
+            else
+                frame = 0;
         }
     }
 }
